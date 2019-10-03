70878c9d6545c0f7f0344377327543285fbede02
Gabriel Rigolon <gabrielrigolon@gmail.com>
2019-10-03 17:27:38 -0300

    Feitas as tabelas Consumidor e Mensagem.

---
+++ Repository/Config/MensagemConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.DTO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Config
+{
+    public class MensagemConfiguration : IEntityTypeConfiguration<Mensagem>
+    {
+        public void Configure(EntityTypeBuilder<Mensagem> builder)
+        {
+            builder.HasKey(pre => pre.Id);
+            builder.HasOne(pre => pre.MensagemDestinatario).WithMany().HasForeignKey(pre => pre.IdDestinatario);
+            builder.HasOne(pre => pre.MensagemRemetente).WithMany().HasForeignKey(pre => pre.IdRemetente);
+
+            builder.Property(pre => pre.CaminhoTxt).IsRequired().HasMaxLength(255).HasColumnType("varchar(255)");
+            builder.Property(pre => pre.Data).HasMaxLength(8).HasColumnType("Date");
+
+        } 
+    }
+}
---
+++ Repository/Repository/MensagemRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Repository;
+using Repository.Context;
+
+namespace Repository.Repository
+{
+    public class MensagemRepository : BaseRepository<Domain.DTO.Mensagem>, IMensagemRepository
+    {
+        public MensagemRepository(HelpIncContext helpIncContext) : base(helpIncContext)
+        {
+
+        }
+    }
+}
