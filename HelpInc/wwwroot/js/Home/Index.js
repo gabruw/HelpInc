ca5469d668bb009beb541097ae882b9780055255
gabrielrigolon <44919590+gabrielrigolon@users.noreply.github.com>
2019-10-09 00:21:06 -0300

    Carousel Adicionado ao projeto

--- HelpInc/wwwroot/js/Home/Index.js
+++ HelpInc/wwwroot/js/Home/Index.js
@@ -18,4 +18,36 @@
             hide: 300
         }
     });
+
+    new Glide('#Glide-container', {
+    type: 'carousel',
+    focusAt: 'center',
+    gap: 10,
+    perView: 1,
+    startAt: 0,
+    autoplay: 3000,
+    hoverpause: true,
+    rewind: true,
+    rewindDuration: 8000,
+    animationDuration: 600,
+    animationTimingFunc: 'ease-out',
+    activeNav: 'glide__bullet--active',
+  }).mount();
+
+
+  let divBullet = document.querySelector(".glide__bullets");
+  let slides = document.querySelector(".glide__slides");
+  let cont = 0;
+
+  slides.childNodes.forEach((value) => {
+    if (value.className === "glide__slide" || cont === 0) {
+      cont++;
+      
+      const button = document.createElement('button');
+      button.setAttribute('class', 'glide__bullet');
+      button.setAttribute('data-glide-dir', `=${cont}`);
+
+      divBullet.appendChild(button);
+    }
+  });
 });
