c1130a9adf07da0ed3b3fbcc59b406095fb91bd9
gabriel.franca <gabriel.franca@ntendencia.com.br>
2019-10-15 15:28:22 -0300

    [5.2][Add]_Mais elementos

---
+++ HelpInc/wwwroot/js/Generic/Rating.js
@@ -0,0 +1,10 @@
+﻿$('.ui.rating.non-interactive').rating({
+    interactive: false,
+    maxRating: 5
+});
+
+$('.ui.rating.interactive').rating({
+    interactive: true,
+    initialRating: 0,
+    maxRating: 5
+});
