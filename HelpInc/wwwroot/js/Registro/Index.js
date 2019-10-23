5737d99e1486c6f0e2c856443f4a53baa64f36ba
JovanyTvares <jovan.tav@gmail.com>
2019-10-23 19:37:34 -0300

    [4.1][Registrar] - Criar Layout
    Falta principalmente o js para o campo cidade e o campo que definirá o que cada prestador ou empresa faz. tmj

--- HelpInc/wwwroot/js/Registro/Index.js
+++ HelpInc/wwwroot/js/Registro/Index.js
@@ -1,4 +1,22 @@
-﻿$('#Cidade')
+﻿$(document).ready(function () {
+    randomBackgroundImage = () => {
+        let vectorImage = ["url('../images/Login/background-login-1.jpg')",
+            "url('../images/Login/background-login-2.jpg')",
+            "url('../images/Login/background-login-3.jpg')",
+            "url('../images/Login/background-login-4.jpg')",
+            "url('../images/Login/background-login-5.jpg')",
+            "url('../images/Login/background-login-6.jpg')"
+        ];
+
+        const imageRandom = Math.floor(Math.random() * vectorImage.length);
+        $("body").css("background-image", vectorImage[imageRandom]);
+    };
+});
+
+$('#Estado')
+    .dropdown();
+
+$('#Cidade')
     .dropdown();
 
 $('#tela-registro .menu .item').tab();
